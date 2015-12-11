13e481e1224f67c1c95c0dfb7f931df73284bf8b
TillHemmerich <till.hemmerich@hotmail.de>
2015-12-11 20:14:57 +0100

    Still sprite shit

--- 2dBeatEmUp/src/pkg2dbeatemup/Player.java
+++ 2dBeatEmUp/src/pkg2dbeatemup/Player.java
@@ -13,5 +13,16 @@ import java.awt.image.BufferedImage;
  */
 public class Player {
 
+  private BufferedImage[] walkingLeft = {Sprite.getSprite(0, 1), Sprite.getSprite(2, 1)}; // Gets the upper left images of my sprite sheet
+private BufferedImage[] walkingRight = {Sprite.getSprite(0, 2), Sprite.getSprite(2, 2)};
+private BufferedImage[] standing = {Sprite.getSprite(1, 0)};
 
+
+//private Animation walkLeft = new Animation(walkingLeft, 10);
+//private Animation walkRight = new Animation(walkingRight, 10);
+//private Animation standing = new Animation(standing, 10);
+//
+//
+//private Animation animation = standing;
+   
 }
