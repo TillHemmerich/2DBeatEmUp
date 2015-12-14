d9a6b40ba523e007c21f545d042581b0d5333f72
Philipp Schumann <philippschumann97@gmail.com>
2015-12-14 22:34:21 +0100

    added background scrolling

--- 2dBeatEmUp/src/pkg2dbeatemup/gamePanel.java
+++ 2dBeatEmUp/src/pkg2dbeatemup/gamePanel.java
@@ -28,7 +28,9 @@ import javax.swing.JPanel;
  */
 public class gamePanel extends JPanel {
 
-    int iMax = 28;
+    int backgroundX;
+    int characterMovementSpeed = 28;
+    int backgroundScrollSpeed = 6;
     private Player player;
     private Image image = new ImageIcon("Resources/bilder/2d_background.jpg").getImage();
     private Rectangle ground = new Rectangle(0, 0, 0, 0);
@@ -36,15 +38,16 @@ public class gamePanel extends JPanel {
     public gamePanel() {
         player = new Player();
         this.setSize(image.getHeight(this), image.getWidth(this));
+        backgroundX = 0;
         new Thread(new Runnable() {
             @Override
             public void run() {
                 int i = 0;
                 while (true) {
-
-                    if (i < iMax) {
+                    //character
+                    if (i < characterMovementSpeed) {
                         i++;
-                    } else if (i == iMax) {
+                    } else if (i == characterMovementSpeed) {
                         i = 0;
                     }
                     switch (player.getCurrentState()) {
@@ -55,27 +58,40 @@ public class gamePanel extends JPanel {
                             player.setCurrentImage(Sprite.getSprite(1, 2));
                             break;
                         case Player.STATE_WALKING_LEFT:
-                            if (i <= ((double) iMax * 0.33)) {
+                            if (i <= ((double) characterMovementSpeed * 0.33)) {
                                 player.setCurrentImage(Sprite.getSprite(0, 1));
-                            } else if (i <= ((double) iMax * 0.66)) {
+                            } else if (i <= ((double) characterMovementSpeed * 0.66)) {
                                 player.setCurrentImage(Sprite.getSprite(1, 1));
-                            } else if (i <= (double) iMax) {
+                            } else if (i <= (double) characterMovementSpeed) {
                                 player.setCurrentImage(Sprite.getSprite(2, 1));
                             }
 
                             break;
                         case Player.STATE_WALKING_RIGHT:
-                            if (i <= ((double) iMax * 0.33)) {
+                            if (i <= ((double) characterMovementSpeed * 0.33)) {
                                 player.setCurrentImage(Sprite.getSprite(0, 2));
-                            } else if (i <= ((double) iMax * 0.66)) {
+                            } else if (i <= ((double) characterMovementSpeed * 0.66)) {
                                 player.setCurrentImage(Sprite.getSprite(1, 2));
-                            } else if (i <= (double) iMax) {
+                            } else if (i <= (double) characterMovementSpeed) {
                                 player.setCurrentImage(Sprite.getSprite(2, 2));
                             }
                             break;
                         default:
                             player = null;
                     }
+                    //background
+                    if (backgroundX < getWidth() || backgroundX > 0) {
+                        if (player.getCurrentState().equals(Player.STATE_WALKING_RIGHT)) {
+                            backgroundX -= backgroundScrollSpeed;
+                        } else if (player.getCurrentState().equals(Player.STATE_WALKING_LEFT)) {
+                            backgroundX += backgroundScrollSpeed;
+                        }
+
+                    }
+                    if (backgroundX >= getWidth() || backgroundX <= 0 - getWidth()) {
+                        backgroundX = 0;
+                    }
+
                     repaint();
                     try {
                         Thread.sleep(1000 / 60);
@@ -93,19 +109,22 @@ public class gamePanel extends JPanel {
 
     @Override
     public void paint(Graphics g) {
+        g.clearRect(0, 0, getWidth(), getHeight());
         paintBackground((Graphics2D) g);
         paintGround((Graphics2D) g);
         paintPlayer((Graphics2D) g, player.getCurrentState());
     }
 
     public void paintBackground(Graphics2D g) {
-        g.drawImage(image, 0, 0, null);
+        g.drawImage(image, backgroundX, 0, null);
+        g.drawImage(image, backgroundX - getWidth(), 0, null);
+        g.drawImage(image, backgroundX + getWidth(), 0, null);
 
     }
 
     public void paintGround(Graphics2D g) {
         ground.setSize(getWidth(), 50);
-        ground.setLocation(0, getHeight());
+        ground.setLocation(0, getHeight() - 68);
         g.fill(ground);
 
     }
